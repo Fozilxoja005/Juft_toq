bfc52161ca1f1b63a7e2702136738fc08e60ca97
Fozilxoja <fozilxoja@email.com>
2022-04-03 13:16:39 +0500

    juft_toq

---
+++ script.js
@@ -0,0 +1,11 @@
+do {
+    var son = +prompt('son kiriting')
+} while (isNaN(son) || son == 0)
+let javob = son % 2
+console.log(javob);
+if (javob == 0) {
+    
+    alert('juft')
+} else {
+    alert('toq')
+}
